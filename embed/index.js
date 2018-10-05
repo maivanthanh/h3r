a2a4b725e1d2267a58aea1101e15ab08212ce53c
giangm9 <giangm9@gmail.com>
2018-10-05 19:22:30 +0700

    simpify

--- embed/index.js
+++ embed/index.js
@@ -2,5 +2,9 @@ $(function() {
     var clip = new h3r.Clip(document.getElementById("main"));
     let urlParams = new URLSearchParams(window.location.search);
     let h3rURL = urlParams.get('url');
-    clip.appendH3R(h3rURL);
+    if (!h3rURL) {
+        console.error("format: https://h3rembed.vn?url=<your_url_here>");
+    } else {
+        clip.appendH3R(h3rURL);
+    }
 });
