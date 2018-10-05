720f757de750da9c91770e3d48fff813c111781c
giangm9 <giangm9@gmail.com>
2018-10-05 19:21:42 +0700

    better convention

--- src/Clip.js
+++ src/Clip.js
@@ -4,11 +4,6 @@ import { OrbitControls } from "./libs/OrbitControls"
 
 const gltfLoader = new GLTFLoader();
 
-var ClipState = {
-    PLAY: 1,
-    PAUSE: 2
-}
-
 /**
  * Assign clip to a canvas
  * @param {HTMLCanvasElement} container
@@ -19,163 +14,56 @@ function Clip(container) {
         return;
     }
 
-    /** @type {HTMLCanvasElement} */
-    this.container = container;
-
-    /** @type {THREE.WebGLRenderer} */
-    this.renderer = null;
-
-    /** @type {THREE.Clock} */
-    this.clock = new THREE.Clock();
-
-    /** @type {THREE.Scene} */
-    this.scene = new THREE.Scene();
-
-    /** @type {THREE.PerspectiveCamera} */
-    this.camera = null;
-
-    /** @type {THREE.Mesh} */
-    this.cube = null;
-
+    /** @public PUBLIC */
     this.duration = 0;
-    this.time = 0;
-    this.state = ClipState.PAUSE;
-    this.actions = [];
-    this.mixers = [];
+    this.time =  0;
+    this.state = 'pause'
+    // this.play = play;
+    // this.pause = pause;
+    this.toggle = () => { this.state == 'pause' ? play() : pause(); }
+    this.appendH3R = (url) => {
 
-
-    /** @private **/
-
-    /** @type {OrbitControl} */
-    this._controls = null;
-
-    this._initCamera();
-    this._initScene();
-    this._initRenderer();
-    this._initControls();
-    this._initResize();
-    this._initLight();
-    this._animate();
-}
-
-/**
- * Override h3r data
- * @param {string} url
- */
-
-Clip.prototype.appendH3R = function (url) {
-    var context = this;
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            var serverResponse = this.responseText;
-            var h3r = JSON.parse(serverResponse);
-            h3r.gltf.forEach(function (gltf) {
-                context.appendGLTF(gltf);
-            });
-        }
-    }
-
-    xhttp.open("GET", url, true);
-    xhttp.send();
-}
-
-
-/**
- * Add gltf from url  ( no cross origin )
- * @param {string} url
- */
-
-Clip.prototype.appendGLTF = function (url) {
-    var context = this;
-    var delta = this.clock.getDelta();
-    gltfLoader.load(url, function (gltf) {
-        gltf.scene.traverse(function (object) {
-            object.frustumCulled = false;
-            if (object.isMesh) {
-                object.material.side = THREE.DoubleSide;
-                object.castShadow = true;
-                object.receiveShadow = true;
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                var serverResponse = this.responseText;
+                var h3r = JSON.parse(serverResponse);
+                h3r.gltf.forEach(function (gltf) {
+                    appendGLTF(gltf);
+                });
             }
-        });
-        context.scene.add(gltf.scene);
+        }
 
-        var mixer = new THREE.AnimationMixer(gltf.scene);
-        context.mixers.push(mixer);
-        if (!gltf.animations) return;
+        xhttp.open("GET", url, true);
+        xhttp.send();
 
-        gltf.animations.forEach(function (anim) {
-            var action = mixer.clipAction(anim);
-            context.actions.push(action);
-            action.play();
-        });
-
-
-    })
-}
-
-/** @private **/
-
-Clip.prototype._size = function () {
-    return {
-        width: this.container.clientWidth,
-        height: this.container.clientHeight,
-        ratio: this.container.clientWidth / this.container.clientHeight
     }
-}
 
+    /** @private PRIVATE */
 
-Clip.prototype._initLight = function () {
+    var container = container,
+        renderer = null,
+        clock = new THREE.Clock(),
+        scene = new THREE.Scene(),
+        camera = null,
+        actions = [],
+        mixers = [],
+        controls = null;
 
-    var light = new THREE.HemisphereLight(0xffffbb, 0x080820, 2);
-    this.scene.add(light);
-}
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-Clip.prototype._initCamera = function () {
-    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-}
+    camera.position.z = 75;
+    camera.position.y = 30;
 
-Clip.prototype._initScene = function () {
-
-    // var geometry = new THREE.BoxGeometry(1, 1, 1);
-    // var material = new THREE.MeshPhongMaterial({ color: 0xffffff});
-    // this.cube = new THREE.Mesh(geometry, material);
-    // this.scene.add(this.cube);
-
-    this.camera.position.z = 75;
-    this.camera.position.y = 30;
-
-}
-
-Clip.prototype._initRenderer = function () {
-    this.renderer = new THREE.WebGLRenderer(
+    renderer = new THREE.WebGLRenderer(
         {
             antialias: true,
             alpha: true,
-            canvas: this.container
+            canvas: container
         }
     );
-    this.renderer.setSize(this._size().width, this._size().height, false);
-}
-
-
-Clip.prototype._animate = function () {
-    var delta = this.clock.getDelta();
-    requestAnimationFrame(this._animate.bind(this));
-
-    this.renderer.render(this.scene, this.camera);
-    this._controls.update();
-    this.mixers.forEach((mixer) => { mixer.update(0); });
-
-    this.actions.forEach(function (action) {
-        action.time += delta;
-    });
-
-
-}
-
-Clip.prototype._initControls = function () {
-    var controls = new OrbitControls(this.camera, this.container);
+    renderer.setSize(size().width, size().height, false);
+    controls = new OrbitControls(camera, container);
 
     controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
     controls.dampingFactor = 0.25;
@@ -186,19 +74,67 @@ Clip.prototype._initControls = function () {
     controls.maxDistance = 100;
 
     controls.maxPolarAngle = Math.PI / 2;
-    this._controls = controls;
-}
-
-Clip.prototype._initResize = function () {
-    var context = this;
     window.addEventListener('resize', onWindowResize, false);
 
     function onWindowResize() {
 
-        context.camera.aspect = context._size().ratio;
-        context.camera.updateProjectionMatrix();
+        camera.aspect = size().ratio;
+        camera.updateProjectionMatrix();
 
-        context.renderer.setSize(context._size().width, context._size().height, false);
+        context.renderer.setSize(size().width, size().height, false);
+
+    }
+
+    var light = new THREE.HemisphereLight(0xffffbb, 0x080820, 2);
+    scene.add(light);
+
+    animate();
+
+    function animate() {
+        var delta = clock.getDelta();
+        requestAnimationFrame(animate);
+
+        renderer.render(scene, camera);
+        controls.update();
+        mixers.forEach((mixer) => { mixer.update(0); });
+
+        actions.forEach(function (action) {
+            action.time += delta;
+        })
+    }
+
+    function appendGLTF(url) {
+
+        gltfLoader.load(url, function (gltf) {
+            gltf.scene.traverse(function (object) {
+                object.frustumCulled = false;
+                if (object.isMesh) {
+                    object.material.side = THREE.DoubleSide;
+                    object.castShadow = true;
+                    object.receiveShadow = true;
+                }
+            });
+            scene.add(gltf.scene);
+
+            var mixer = new THREE.AnimationMixer(gltf.scene);
+            mixers.push(mixer);
+            if (!gltf.animations) return;
+
+            gltf.animations.forEach(function (anim) {
+                var action = mixer.clipAction(anim);
+                actions.push(action);
+                action.play();
+            });
+
+        })
+    }
+
+    function size() {
+        return {
+            width: container.clientWidth,
+            height: container.clientHeight,
+            ratio: container.clientWidth / container.clientHeight
+        }
 
     }
 }
